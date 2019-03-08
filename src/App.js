0bfd4a9697943c817f6878959c7482ce1638868d
yan wu <yanwu@yans-MBP.fios-router.home>
2019-03-07 21:23:57 -0500

    update css and data source

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
 import MyList from './component/MyList';
 import Recommendation from './component/Recommendation';
+import image from './image/netflix-logo.jpg';
+import { connect } from "react-redux";
 import './App.css';
+
 
 
 
 class App extends Component {
 
+  componentDidMount(){
+    fetch('./data.json')
+    .then(res => res.json())
+    .then(res => this.props.Initial(res))
+ }
+
   render() {
     return (
-      <div className='layout'>
-        <MyList />
-        <Recommendation />
-               
-      </div>
+      <>
+        <header>
+              <img className='label' src={image} alt='Netflex'/>
+        </header>
+        <div className='layout'>
+
+          <MyList />
+          <Recommendation />
+                
+        </div>
+      </>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+      Initial: (data) => dispatch({type: 'INITIAL', payload: data})
+  };
+}
+
+export default connect(null,mapDispatchToProps)(App);
